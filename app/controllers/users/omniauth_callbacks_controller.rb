13927a61b2a5ea1ccf898e08a2292cb918fad028
Lowell Mower <lowell.m.mower@gmail.com>
2015-10-21 22:02:12 -0400

    control flow and session setting after persisting check

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,10 +4,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
+      flash.notice = "Signed in!"
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "twitter") if is_navigational_format?
     else
-      session["devise.twitter_data"] = request.env["omniauth.auth"]
+      session["devise.user_attributes"] = @user.attributes
       redirect_to new_user_registration_url
     end
   end
