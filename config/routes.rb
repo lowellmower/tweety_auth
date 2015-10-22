35e7621f8c3f97c9c327a7d11f629b0f27fd6a02
Lowell Mower <lowell.m.mower@gmail.com>
2015-10-22 19:26:37 -0400

    add routing for terms and privacy

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  get '/terms' => 'welcome#terms'
+  get 'privacy' => 'welcome#privacy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
