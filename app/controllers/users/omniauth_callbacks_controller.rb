9b8d8a86ffd09b326af0915f85956f1725ccb0ec
Lowell Mower <lowell.m.mower@gmail.com>
2015-10-23 18:29:19 -0400

    helpful commenting

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,9 +4,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
+      # if all information is avail from auth, signin and redirect
       flash.notice = "Signed in!"
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      sign_in_and_redirect @user, :event => :authentication #throw if @user is not activated
     else
+      # save what has come from auth in session and send to signup
       session["devise.user_attributes"] = @user.attributes
       redirect_to new_user_registration_url
     end
